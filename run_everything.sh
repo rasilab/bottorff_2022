ec328dafb51c58cd4bfab69e11cb0857a1105519
rasi <rasi1983@gmail.com>
2022-01-22 23:33:36 -0800

    singularity container working well now for cluster submission

--- run_everything.sh
+++ run_everything.sh
@@ -1,12 +1,34 @@
-WORKSPACE="."
-# experiments
+# Script for running all simulations and analyses
+
+echo $@
+
+if [ $1 = "--use-singularity" ]
+then
+    SINGULARITY_ARGS="--use-singularity"
+else
+    SINGULARITY_ARGS=""
+fi
+
+if [ $2 = "--use-cluster" ]
+then
+    CLUSTER_ARGS="-p --jobs 999 --cluster-config ../../../.snakemake_profile/cluster.yaml --restart-times 2 --nolock --cluster"
+    SLURM_COMMAND="sbatch -n {cluster.n}  -t {cluster.time}"
+else
+    CLUSTER_ARGS="-p --cores=8"
+    SLURM_COMMAND=""
+fi
+
+# Experiments
 
 # figure number panel
-cd $WORKSPACE/experiments/plate_reader/EXPERIMENT_NAME
-snakemake
+# cd $WORKSPACE/experiments/plate_reader/EXPERIMENT_NAME
+# snakemake
 
 # simulations
 
-# figure number panel
-cd $WORKSPACE/simulation/simulations_runs/SIMULATION_NAME
-snakemake
+# figure 2C
+cd modeling/simulation_runs/0a_ivanov_geballe_models_vary_reinit_init_for_fitting_controls/
+# Note that we should first analyze the experimental data and copy it to the
+# relevant directory before running the analyze_results.ipynb script. Otherwise
+# the script does not find data outside its parent directory
+snakemake $CLUSTER_ARGS "$SLURM_COMMAND" $SINGULARITY_ARGS
